6128c06c416f4b756fdc15a2f2f0a959f48806e3
roniitkhanna <roniitkhanna@gmail.com>
2024-11-22 23:09:12 +0530

    added profile page and edit profile func.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './components/Login'
 import MainLayout from './components/MainLayout'
 import Home from './components/Home'
 import Profile from './components/Profile'
+import EditProfile from './components/EditProfile'
 
 function App() {
   const browserRouter = createBrowserRouter([
@@ -19,7 +20,11 @@ function App() {
         {
           path:'/profile/:id',
           element:<Profile/>
-        }
+        },
+        {
+          path:'/account/edit',
+          element:<EditProfile/>
+        },
       ]
     },
     {
